04b58ee20d8c6fce2359759322dbc4edfb7eb75d
Dave Braunschweig <dave@chicagocoding.com>
2020-04-20 14:14:04 -0500

    Added atm property and method

--- atm.py
+++ atm.py
@@ -1,5 +1,15 @@
 # This is the main ATM program, this will call all other classes and modules.
 
 class Atm(object):
+    _id = "12345"
+
+    @property
+    def id(self):
+        return self._id
+
     def __init__(self):
         print("Atm initialized")
+
+    def display_welcome(self):
+        # console = console_library.Console()
+        print("Welcome to our very expensive bank!")
