cd7f37c3e07d8438d1c2529dfc89f8bbe523d5c7
Jacob Narayan <j_narayan1@mail.harpercollege.edu>
2020-04-20 15:42:50 -0500

    commented out my additions

--- atm.py
+++ atm.py
@@ -1,54 +1,64 @@
 # This is the main ATM program, this will call all other classes and modules.
-import session as session_library
-import card_reader as card_reader_library
-import customer_console as customer_console_library
-import operator_panel as operator_panel_library
-import log as log
-import account as acount_library
-import network as network_library
-import receipt as receipt_library
+
+# import session as session_library
+# import card_reader as card_reader_library
+# import customer_console as customer_console_library
+# import operator_panel as operator_panel_library
+# import log
+# import account as acount_library
+# import network as network_library
+# import receipt as receipt_library
 
 class Atm(object):
     _id = "12345"
 
-    @property
-    def id(self):
-        return self._id
+    # @property
+    # def id(self):
+    #     return self._id
+
+    # @property
+    # def card_number(self):
+    #     return self.card_number
+    
+    # @card_number.setter
+    # def card_number(self):
+    #     self.card_number = self.read_card()
 
     def __init__(self):
         print("Atm initialized")
+        self.display_welcome()
 
     def display_welcome(self):
         # console = console_library.Console()
         print("Welcome to our very expensive bank!")
 
-    def read_card(self):
-        self.card_number = card_reader_library.Get_Card()
+    # def read_card(self):
+    #     return card_reader_library.get_card()
 
-    def start_session(self):
-        self.session = session_library.Session(self._id, self.card_number)
-        print("Session started.")
+    # def start_session(self):
+    #     self.session = session_library.Session(self._id, self.card_number)
+    #     print("Session started.")
 
-    def end_session(self):
-        self.session.end_session(self.session)
-        print("Session Ended.")
+    # def end_session(self):
+    #     self.session.end_session(self.session)
+    #     print("Session Ended.")
     
-    def start_customer_console(self):
-        self.customer_console = customer_console_library.Console()
-        print("Console started.")
+    # def start_customer_console(self):
+    #     self.customer_console = customer_console_library.Console()
+    #     print("Console started.")
     
-    def get_action(self):
-        self.action = self.customer_console.get_action()
+    # def get_action(self):
+    #     self.action = self.customer_console.get_action()
 
-    def deposit_withdrawal_transfer(self):
-        if self.action == "deposit":
-            continue
-        elif self.action == "withdrawal":
-            continue
-        elif self.actioin == "transfer":
-            continue
-        else:
-            print("Invalid action, please try again.")
+    # def deposit_withdrawal_transfer(self):
+    #     if self.action == "deposit":
+    #         continue
+    #     elif self.action == "withdrawal":
+    #         continue
+    #     elif self.actioin == "transfer":
+    #         continue
+    #     else:
+    #         print("Invalid action, please try again.")
             
 
 
--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import atm as atm_library
 
 if __name__ == "__main__":
     atm = atm_library.Atm()
-    atm.read_card()    
-    atm.start_session(atm._id, atm.card_number)
-    atm.start_customer_console()
-    atm.get_action()
+    # atm.read_card()    
+    # atm.start_session(atm._id, atm.card_number)
+    # atm.start_customer_console()
+    # atm.get_action()
+    #depost
     time.sleep(1)
