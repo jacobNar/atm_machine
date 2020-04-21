5eb82df2afb3461f0ee514119fd2a9763178c0f4
Zachary Bogdanski <zbogdanski@gmail.com>
2020-04-21 14:08:33 -0500

    Updated customer module

--- customer.py
+++ customer.py
@@ -1,16 +1,21 @@
 # Customer module to handle customer information
 
 class Customer(object):
+    _name = None
+    _card_id = None
+    _pin = None
+    _account_number = None
+
     def __init__(self, name, card_id, pin, account_number):
-        self.name = name
-        self.card_id = card_id
-        self.pin = pin
-        self.account_number = account_number
+        self._name = name
+        self._card_id = card_id
+        self._pin = pin
+        self._account_number = account_number
         print("Customer created")
 
 
     def validate_card_id_pin(self, card_id, pin):
-        if card_id == self.card_id and pin == self.pin:
+        if card_id == self._card_id and pin == self._pin:
             return self.account_number # or return True if validated?
             
 
---
+++ test_customer.py
@@ -0,0 +1,10 @@
+import pytest
+
+import atm as atm_library
+
+def test_init():
+    customer = customer.Customer()
+    assert customer != None
+
+def test_validate_card_id_pin(capsys):
+    pass
