46e9270b81323eebf5e669b80ef1ce2d9a935a8d
Dave Braunschweig <dave@chicagocoding.com>
2020-04-22 13:32:34 -0500

    customer properties example

--- customer.py
+++ customer.py
@@ -6,18 +6,24 @@ class Customer(object):
     _pin = None
     _account_number = None
 
-    def __init__(self, name, card_id, pin, account_number):
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    def __init__(self, name="", card_id="", pin="", account_number=""):
         self._name = name
         self._card_id = card_id
         self._pin = pin
         self._account_number = account_number
         print("Customer created")
 
-
     def validate_card_id_pin(self, card_id, pin):
         if card_id == self._card_id and pin == self._pin:
             return self._account_number # or return True if validated?
             
-
     def get_customer_info(self, name, card_id, pin, account_number):
         pass
--- test_customer.py
+++ test_customer.py
@@ -1,10 +1,14 @@
 import pytest
-
-import atm as atm_library
+import customer
 
 def test_init():
-    customer = customer.Customer()
-    assert customer != None
+    _customer = customer.Customer()
+    assert _customer != None
+
+def test_name():
+    _customer = customer.Customer()
+    _customer.name = "Test"
+    assert _customer.name == "Test"
 
 def test_validate_card_id_pin(capsys):
     pass
