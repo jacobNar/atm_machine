d134e2ad6e802c13bd1121585b1df532a635e24b
w_workman1 <w_workman1@mail.harpercollege.edu>
2020-04-21 20:52:10 -0500

    Updated code for console

--- console.py
+++ console.py
@@ -1,19 +1,81 @@
-# Gets user input for ATM
+import sys
+
+# Gets user input and output for ATM.
 
 
-class Console:
-    def __init__(self, pin_number, menu_choice, amount):
-        self.pin = pin_number
-        self.choice = menu_choice
+class Console(object):
+    amount = 0
+
+    def __init__(self, pin, choice, amount, id):
+        """ Creates Console object
+        :param pin:
+        :param choice:
+        :param amount:
+        :param id:
+        """
+        self.pin = pin
+        self.choice = choice
         self.amount = amount
+        self.id = id
+        print("Console created")
 
 
-def pin():
-    pin_number = int(input("Enter pin: "))
-    print(pin_number)
+def get_pin():
+    """ Asks user for PIN number
+    :return pin:
+    """
+    pin = int(input("Enter PIN: "))
+    if pin < 1000 or pin > 9999:
+        print("Invalid PIN")
+        pin = None  # Might add loop if pin = None so user can enter valid PIN.
+    else:
+        print("Valid PIN!")
+        # pin = True
+    print(pin)  # An ATM usually does not print PIN. Printing PIN to make sure input works. Will remove it.
+    return pin
 
 
-def menu():
-    print("\n1 - Deposit \t 2 - Withdraw \t 3 - Transaction")
-    menu_choice = int(input("Enter a number: "))
-    print(menu_choice)
+def get_card_identification():
+    id = int(input("Enter your card ID: "))
+    print(id)  # Printing to make sure input works.
+    # Will add error handling based on what a card ID should look like
+
+
+def get_choice():
+    print("\n1 - Deposit \t 2 - Withdraw \t 3 - Transfer")
+    choice = int(input("Select an option: "))
+    return choice
+
+
+def display_menu(choice):
+    """ Displays menu for deposit, withdraw, and transfer
+    :return:
+    """
+    if choice == 1:
+        deposit = float(input("Enter amount to deposit: "))
+        verify_deposit = input("Is this the correct amount, Yes or No ? " + str(deposit))
+        if verify_deposit == "Y":
+            print("Deposit verified!")
+        else:
+            sys.exit()
+            # Add error handling
+    elif choice == 2:
+        withdraw = float(input("Enter amount to withdraw: "))
+        verify_withdraw = input("Is this the correct amount, Yes or No ? " + str(withdraw))
+        if verify_withdraw == "Y":
+            print("Withdraw verified!")
+        else:
+            sys.exit()
+            # Add error handling
+    elif choice == 3:
+        transfer = float(input("Enter amount to transfer: "))
+        verify_transfer = input("Is this the correct amount, Yes or No ? " + str(transfer))
+        if verify_transfer == "Y":
+            print("Transfer verified!")
+        else:
+            sys.exit()
+            # Add error handling
+
+
+if __name__ == '__main__':
+    console = Console
--- test_console.py
+++ test_console.py
@@ -1,21 +1,49 @@
-import console
+import console as console_library
 
 
-def test_pin():
-    input_values = [1234]
-
-    def input(prompt=None):
-        return input_values.pop()
-
-    console.input = input
-    assert console.pin() == None
+def test_init():
+    console = console_library.Console(1111, 1, 0, 1)
+    assert console is not None
 
 
-def test_menu():
-    input_values = [2]
+def test_get_pin():
+    input_values = [1111]
 
     def input(prompt=None):
         return input_values.pop()
 
-    console.input = input
-    assert console.menu() == None
+    console_library.input = input
+    assert console_library.get_pin() == 1111
+
+
+def test_invalid_pin_over_four_digits():
+    input_values = [999999]
+
+    def input(prompt=None):
+        return input_values.pop()
+
+    console_library.input = input
+    assert console_library.get_pin() is None
+
+
+def test_invalid_pin_under_four_digits():
+    input_values = [1]
+
+    def input(prompt=None):
+        return input_values.pop()
+
+    console_library.input = input
+    assert console_library.get_pin() is None
+
+
+def test_get_choice():
+    input_values = [1]
+
+    def input(prompt=None):
+        return input_values.pop()
+
+    console_library.input = input
+    assert console_library.get_choice() == 1
+
+
+
