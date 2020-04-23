1610def32a3f05c00775389157f8ca6c48963f4e
Zachary Bogdanski <zbogdanski@gmail.com>
2020-04-22 23:45:19 -0500

    Added logout method (destructor) and is_validated attribute for session control

--- customer.py
+++ customer.py
@@ -1,29 +1,45 @@
-# Customer module to handle customer information
+# Customer module to handle customer information such as name, card number, pin, and account number
 
 class Customer(object):
     _name = None
     _card_id = None
     _pin = None
     _account_number = None
+    _is_validated = False
 
     @property
     def name(self):
         return self._name
 
+    @property
+    def is_validated(self):
+        return self._is_validated
+
+    @property
+    def pin(self):
+        return self._pin
+
     @name.setter
     def name(self, value):
-        self._name = value
+        if self._is_validated == True:
+            self._name = value
 
+    @pin.setter
+    def pin(self, value):
+        if self._is_validated == True:
+            self._pin = value
+
+    def logout(self):
+        del self
+        
     def __init__(self, name="", card_id="", pin="", account_number=""):
         self._name = name
         self._card_id = card_id
         self._pin = pin
         self._account_number = account_number
-        print("Customer created")
+        print(f"Customer created. Welcome, {name}!")
 
     def validate_card_id_pin(self, card_id, pin):
         if card_id == self._card_id and pin == self._pin:
-            return self._account_number # or return True if validated?
-            
-    def get_customer_info(self, name, card_id, pin, account_number):
-        pass
+            self._is_validated = True
+            return True
--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+# import time
+
+# import atm as atm_library
+
+# if __name__ == "__main__":
+#     atm = atm_library.Atm()
+#     # atm.read_card()    
+#     # atm.start_session(atm._id, atm.card_number)
+#     # atm.start_customer_console()
+#     # atm.get_action()
+#     # deposit_withdrawal_transfer()
+#     time.sleep(1)
+
 import customer
 import time
 
 if __name__ == "__main__":
-    customer = customer.Customer('testName', 'testCardID', 'testPin', 'testAccount')
+    customer = customer.Customer("bob", 1234, 1234, "")
     time.sleep(1)
+    customer.validate_card_id_pin(1234, 1234)
+    print(customer.name)
+    print(customer.pin)
+    customer.logout()
+    
