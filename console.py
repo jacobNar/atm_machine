49b49124cf0ae2c17bb5c434f8b3133155073740
w_workman1 <w_workman1@mail.harpercollege.edu>
2020-04-22 20:43:04 -0500

    Renamed functions, added verification for user input, get_choice now in console_display_menu, added exit

--- console.py
+++ console.py
@@ -1,81 +1,91 @@
-import sys
-
 # Gets user input and output for ATM.
+import sys
 
 
 class Console(object):
-    amount = 0
 
-    def __init__(self, pin, choice, amount, id):
+    def __init__(self, pin, choice, id):
         """ Creates Console object
         :param pin:
         :param choice:
-        :param amount:
         :param id:
         """
         self.pin = pin
         self.choice = choice
-        self.amount = amount
         self.id = id
         print("Console created")
 
+    def pin(self):
+        return self.pin
 
-def get_pin():
+    def choice(self):
+        return self.choice
+
+    def id(self):
+        return self.id
+
+
+def console_get_pin():
     """ Asks user for PIN number
     :return pin:
     """
-    pin = int(input("Enter PIN: "))
-    if pin < 1000 or pin > 9999:
-        print("Invalid PIN")
-        pin = None  # Might add loop if pin = None so user can enter valid PIN.
-    else:
-        print("Valid PIN!")
-        # pin = True
-    print(pin)  # An ATM usually does not print PIN. Printing PIN to make sure input works. Will remove it.
-    return pin
+    while True:
+        pin = int(input("Enter PIN: "))
+        if pin < 1000 or pin > 9999:
+            print("Invalid PIN")
+            continue
+        else:
+            print("Valid PIN!")
+        return pin
 
 
-def get_card_identification():
-    id = int(input("Enter your card ID: "))
-    print(id)  # Printing to make sure input works.
-    # Will add error handling based on what a card ID should look like
-
-
-def get_choice():
-    print("\n1 - Deposit \t 2 - Withdraw \t 3 - Transfer")
-    choice = int(input("Select an option: "))
-    return choice
-
-
-def display_menu(choice):
-    """ Displays menu for deposit, withdraw, and transfer
+def console_get_card_identification():
+    """ Gets card identification from user input
     :return:
     """
-    if choice == 1:
-        deposit = float(input("Enter amount to deposit: "))
-        verify_deposit = input("Is this the correct amount, Yes or No ? " + str(deposit))
-        if verify_deposit == "Y":
-            print("Deposit verified!")
-        else:
-            sys.exit()
-            # Add error handling
-    elif choice == 2:
-        withdraw = float(input("Enter amount to withdraw: "))
-        verify_withdraw = input("Is this the correct amount, Yes or No ? " + str(withdraw))
-        if verify_withdraw == "Y":
-            print("Withdraw verified!")
-        else:
-            sys.exit()
-            # Add error handling
-    elif choice == 3:
-        transfer = float(input("Enter amount to transfer: "))
-        verify_transfer = input("Is this the correct amount, Yes or No ? " + str(transfer))
-        if verify_transfer == "Y":
-            print("Transfer verified!")
-        else:
-            sys.exit()
-            # Add error handling
+    id = int(input("Enter card ID: "))
+    print("Valid card ID!")
+    return id
+    # Will add error handling based on what ID should look like
 
 
-if __name__ == '__main__':
-    console = Console
+def console_display_menu():
+    """ Gets user input and displays menu for deposit, withdraw, and transfer
+    :return:
+    """
+    print("\n1 - Deposit \t 2 - Withdraw \t 3 - Transfer \t 4 - Exit")
+    choice = int(input("Select an option: "))
+    if choice == 4:
+        print("Thank you for choosing our very expensive bank. Have a nice day!")
+        sys.exit()
+    while True:
+        if choice == 1:
+            deposit = float(input("Enter amount to deposit: "))
+            verify_deposit = input("Is this the correct amount, Yes or No?:  " + '$' + str(deposit))
+            if verify_deposit == "Y" or verify_deposit == "y":
+                print("Deposit verified!")
+                break
+            else:
+                print("Incorrect deposit amount")
+                continue
+        elif choice == 2:
+            withdraw = float(input("Enter amount to withdraw: "))
+            verify_withdraw = input("Is this the correct amount, Yes or No?: " + '$' + str(withdraw))
+            if verify_withdraw == "Y" or verify_withdraw == "y":
+                print("Withdraw verified!")
+                break
+            else:
+                print("Incorrect withdraw amount")
+                continue
+        elif choice == 3:
+            transfer = float(input("Enter amount to transfer: "))
+            verify_transfer = input("Is this the correct amount, Yes or No?: " + '$' + str(transfer))
+            if verify_transfer == "Y" or verify_transfer == "y":
+                print("Transfer verified!")
+                break
+            else:
+                print("Incorrect transfer amount")
+                continue
+
+# if __name__ == '__main__':
+#     console = Console
--- test_console.py
+++ test_console.py
@@ -2,48 +2,72 @@ import console as console_library
 
 
 def test_init():
-    console = console_library.Console(1111, 1, 0, 1)
+    console = console_library.Console(1111, 1, 0)
     assert console is not None
 
 
-def test_get_pin():
+def test_console_get_pin():
     input_values = [1111]
 
     def input(prompt=None):
         return input_values.pop()
 
     console_library.input = input
-    assert console_library.get_pin() == 1111
+    assert console_library.console_get_pin() == 1111
 
 
-def test_invalid_pin_over_four_digits():
+def test_invalid_pin_over_four_digits_prints_string(capsys):
     input_values = [999999]
 
     def input(prompt=None):
         return input_values.pop()
 
     console_library.input = input
-    assert console_library.get_pin() is None
+    captured = capsys.readouterr()
+    assert captured.out == ""
 
 
-def test_invalid_pin_under_four_digits():
+def test_invalid_pin_under_four_digits_prints_string(capsys):
     input_values = [1]
 
     def input(prompt=None):
         return input_values.pop()
 
     console_library.input = input
-    assert console_library.get_pin() is None
+    captured = capsys.readouterr()
+    assert captured.out == ""
 
 
-def test_get_choice():
+def test_console_card_identification():
+    input_values = [123412341234]
+
+    def input(prompt=None):
+        return input_values.pop()
+
+    console_library.input = input
+    assert console_library.console_get_card_identification() == 123412341234
+
+
+def test_console_card_identification_string(capsys):
+    input_values = [123412341234]
+
+    def input(prompt=None):
+        return input_values.pop()
+
+    console_library.input = input
+    console_library.console_get_card_identification()
+    captured = capsys.readouterr()
+    assert captured.out != "Valid card ID!"
+
+
+def test_console_display_menu_prints_string(capsys):
     input_values = [1]
 
     def input(prompt=None):
         return input_values.pop()
 
     console_library.input = input
-    assert console_library.get_choice() == 1
-
+    captured = capsys.readouterr()
+    assert captured.out == ""
 
 
