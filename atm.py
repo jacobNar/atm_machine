51f20dfe3a29039e514b58f1ac545f10c0bf4986
Jacob Narayan <j_narayan1@mail.harpercollege.edu>
2020-04-27 12:47:30 -0500

    deleted commented out code

--- atm.py
+++ atm.py
@@ -17,14 +17,6 @@ class Atm(object):
     def id(self):
         return self._id
 
-    # @property
-    # def card_number(self):
-    #     return self.card_number
-    
-    # @card_number.setter
-    # def card_number(self):
-    #     self.card_number = self.read_card()
-
     @property
     def balance(self):
         self._account.balance
@@ -35,39 +27,12 @@ class Atm(object):
         self._account = Account()
 
     def display_welcome(self):
-        # console = console_library.Console()
         print("Welcome to our very expensive bank!")
 
     def deposit(self, deposit):
         self._account.deposit(deposit)
 
-    # def read_card(self):
-    #     return console.pin()
-
-    # def start_session(self):
-    #     self.session = session_library.Session(self._id, self.card_number)
-    #     print("Session started.")
-
-    # def end_session(self):
-    #     self.session.end_session(self.session)
-    #     print("Session Ended.")
     
-    # def start_customer_console(self):
-    #     self.customer_console = customer_console_library.Console()
-    #     print("Console started.")
-    
-    # def get_action(self):
-    #     self.action = self.customer_console.get_action()
-
-    # def deposit_withdrawal_transfer(self):
-    #     if self.action == "deposit":
-    #         account.deposit()
-    #     elif self.action == "withdrawal":
-    #         account.withdrawal()
-    #     elif self.actioin == "transfer":
-    #         account.transfer()
-    #     else:
-    #         print("Invalid action, please try again.")
             
 
 
