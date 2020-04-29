01bbb49dfab01da03a7f8afe5bd44f0c1e8b40ea
Zachary Bogdanski <zbogdanski@gmail.com>
2020-04-29 14:58:02 -0500

    changed get functions to properties

--- customer.py
+++ customer.py
@@ -35,7 +35,6 @@ class Customer(object):
     _name = None
     _card_id = None
     _pin = None
-    _account_number = None
     _is_validated = False  #session control: logged in with card/pin
     _accounts = []
 
@@ -46,38 +45,25 @@ class Customer(object):
 
     @property
     def is_validated(self):
-        """is_validated accessor function"""
+        """Session validation accessor function"""
         return self._is_validated
 
-    # @property
-    # def pin(self):
-    #     """pin accessor function"""
-    #     if self._is_validated == True:
-    #         return self._pin
-
-    # @name.setter
-    # def name(self, value):
-    #     """name accessor function"""
-    #     if self._is_validated == True:
-    #         self._name = value
-
-    # @pin.setter
-    # def pin(self, value):
-    #     """pin mutator function"""
-    #     if self._is_validated == True:
-    #         self._pin = value
+    @property
+    def accounts(self):
+        """Name accessor function"""
+        if self._is_validated == True:
+            return self._accounts
 
     def logout(self):  # End session: used if data saved to database eventually
         """Calls destructor to end session"""
         _is_validated = False
         del self
         
-    def __init__(self, name="", card_id="", pin="", account_number="", accounts = []):
+    def __init__(self, name="", card_id="", pin="", accounts = []):
         """Customer constructor: name, card_id, pin, account_number"""
         self._name = name
         self._card_id = card_id
         self._pin = pin
-        self._account_number = account_number
         self._accounts = accounts
         # print(f"Customer created. Welcome, {name}!")
 
@@ -90,17 +76,20 @@ class Customer(object):
             self._is_validated = False
             return False
 
-    def get_account_number(self, card_id):
-        if self._is_validated == True:
-            return self._account_number
-
-    def get_accounts(self, account_number):
-        if self._is_validated == True:
-            return self._accounts
-
     def add_account(self, account_number):
+        """Adds new account number to accounts"""
         if self._is_validated == True:
             self._accounts.append(self._account_number)
             return True
         else:
             return False
+
+    # def get_account_number(self, card_id):
+    #     """Gets account number if card_id and pin are validated"""
+    #     if self._is_validated == True:
+    #         return self._account_number
+
+    # def get_accounts(self, card_id):
+    #     """Gets accounts if card_id and pin are validated"""
+    #     if self._is_validated == True:
+    #         return self._accounts
