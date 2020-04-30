422237ac2ee2b098e17e76af96e7fc08ea56449b
w_workman1 <w_workman1@mail.harpercollege.edu>
2020-04-29 20:10:02 -0500

    Changed classes into functions. Added buttons on side.

--- console_gui.py
+++ console_gui.py
@@ -1,128 +1,83 @@
 import tkinter as tk
-from tkinter import font as tkfont
 from tkinter import *
 
 
 def numpad(value):
     global numbers
-
-    numbers += value
-    entry.insert('end', value)
-    print(numbers)
+    if value == '←':
+        numbers = numbers[:-1]
+        entry.delete('0', 'end')
+        entry.insert('end', numbers)
+    else:
+        numbers += value
+        entry.insert('end', value)
+        print(numbers)
 
 
 keys = [
     ['7', '8', '9'],
     ['4', '5', '6'],
     ['1', '2', '3'],
-    ['', '0', '.']
+    ['←', '0', '.']
 ]
 numbers = ''
 
 root = tk.Tk()
-root.title("ATM Number Pad")
+deposit = Frame(root)
+withdraw = Frame(root)
+transfer = Frame(root)
+account = Frame(root)
+
 entry = tk.Entry(root)
 entry.grid(row=0, column=0, columnspan=3, ipady=5)
 for y, row in enumerate(keys, 1):
     for x, key in enumerate(row):
         buttons = tk.Button(root, text=key, command=lambda val=key: numpad(val))
         buttons.grid(row=y, column=x, ipadx=10, ipady=10)
+    for frame in (deposit, withdraw, transfer, account):
+        frame.grid(row=0, column=0)
+    button1 = tk.Button(root, text="Deposit", command=lambda: deposit())
+    button2 = tk.Button(root, text="Withdraw", command=lambda: withdraw())
+    button3 = tk.Button(root, text="Transfer", command=lambda: transfer())
+    button4 = tk.Button(root, text="Account", command=lambda: account())
+    button1.grid(row=1, column=4)
+    button2.grid(row=2, column=4)
+    button3.grid(row=3, column=4)
+    button4.grid(row=4, column=4)
 
-    b = Button(root, text="Enter", width=10, height=2)
-    c = Button(root, text="Clear", width=10, height=2, command=entry.delete(0, END))
-    b.grid(row=1, column=4, sticky=W)
-    c.grid(row=2, column=4, sticky=W)
+
+def deposit():
+    window = Tk()
+    label = tk.Label(window, text="Deposit", font=('Helvetica', 18), pady=10)
+    label.grid(pady=10)
+    # Put in prompt and entry
+    window.mainloop()
+
+
+def withdraw():
+    window = Tk()
+    label = tk.Label(window, text="Withdraw", font=('Helvetica', 18), pady=10)
+    label.grid(pady=10)
+    # Put in prompt and entry
+    window.mainloop()
+
+
+def transfer():
+    window = Tk()
+    label = tk.Label(window, text="Transfer", font=('Helvetica', 18), pady=10)
+    label.grid(pady=10)
+    # Put in prompt and entry
+    window.mainloop()
+
+
+def account():
+    window = Tk()
+    label = tk.Label(window, text="Account", font=('Helvetica', 18), pady=10)
+    label.grid(pady=10)
+    # Put in prompt and entry
+    window.mainloop()
+
+
 root.mainloop()
-
-
-class SampleApp(tk.Tk):
-
-    def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
-
-        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
-
-        container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand=True)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
-
-        self.frames = {}
-        for F in (MainPage, Deposit, Withdraw, Transfer, Account):
-            page_name = F.__name__
-            frame = F(parent=container, controller=self)
-            self.frames[page_name] = frame
-            frame.grid(row=0, column=0, sticky="nsew")
-
-        self.show_frame("MainPage")
-
-    def show_frame(self, page_name):
-        """Shows a frame for the given page name"""
-        frame = self.frames[page_name]
-        frame.tkraise()
-
-
-class MainPage(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        button1 = tk.Button(self, text="Deposit", command=lambda: controller.show_frame("Deposit"))
-        button2 = tk.Button(self, text="Withdraw", command=lambda: controller.show_frame("Withdraw"))
-        button3 = tk.Button(self, text="Transfer", command=lambda: controller.show_frame("Transfer"))
-        button4 = tk.Button(self, text="Account", command=lambda: controller.show_frame("Account"))
-        button1.pack()
-        button2.pack()
-        button3.pack()
-        button4.pack()
-
-
-class Deposit(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, text="Deposit", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Return", command=lambda: controller.show_frame("MainPage"))
-        button.pack()
-
-
-class Withdraw(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, text="Withdraw", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Return", command=lambda: controller.show_frame("MainPage"))
-        button.pack()
-
-
-class Transfer(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, text="Transfer", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Return", command=lambda: controller.show_frame("MainPage"))
-        button.pack()
-
-
-class Account(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, text="Account", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Return", command=lambda: controller.show_frame("MainPage"))
-        button.pack()
-
-
-if __name__ == "__main__":
-    app = SampleApp()
-    app.mainloop()
 
 
