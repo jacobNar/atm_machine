7bbfb1c5294a8203c5fc460e8e4c32c139b2c8a5
Dave Braunschweig <dave@chicagocoding.com>
2020-04-30 11:46:56 -0500

    Change error messages to raise, fix withdraw logic

--- account.py
+++ account.py
@@ -21,7 +21,8 @@ class Account(object):
     def balance(self, value):
         self._balance = value
 
-    def __init__(self, account_number=123, balance=0,
+    def __init__(self, account_number=0, 
+                 balance=0,
                  withdrawal_limit=400):
         """Creates an account object.
 
@@ -35,6 +36,14 @@ class Account(object):
         self._account_number = account_number
         self._withdrawal_limit = withdrawal_limit
 
+    @property
+    def limit(self):
+        return self._withdrawal_limit
+
+    @property
+    def number(self):
+        return self._account_number
+
     def withdrawal(self, withdrawal):
         """Checks if balance is positive, higher than withdrawal amount
         and if withdrawal amount is less than withdrawal limit. If all
@@ -42,33 +51,34 @@ class Account(object):
         the withdrawal. If no, detailed error messages are displayed.
 
         """
-        if self.balance > 0 and \
-            self.balance >= withdrawal and \
-                withdrawal <= self._withdrawal_limit:
-                    self.balance -= withdrawal
-
         if self.balance <= 0:
-            print("You don't have enough funds to complete the transaction." +
+            raise ValueError("You don't have enough funds to complete the transaction."
                   f" Your balance is ${self.balance}.")
+        
         if withdrawal > self.balance:
-            print("You can't withdraw more funds than you have available. " +
-                  f"Your balance is ${self.balance}. You have requested " +
+            raise ValueError("You can't withdraw more funds than you have available. "
+                  f"Your balance is ${self.balance}. You have requested " + \
                   f"${withdrawal}.")
-        if withdrawal > 400:
-            print("Your withdrawal request has been denied. ATM daily " +
-                  "withdrawal limit is $400.00. You have requested " +
+        
+        if withdrawal > self.limit:
+            raise ValueError("Your withdrawal request has been denied. ATM daily "
+                  f"withdrawal limit is {self.limit}. You have requested " + \
                   f"${withdrawal}.")
+        
         if withdrawal <= 0:
-            print("Your withdrawal amount must be a positive number. "
+            raise ValueError("Your withdrawal amount must be a positive number. "
                   f"${withdrawal} is not valid.")
+
+        self.balance -= withdrawal
 
     def deposit(self, deposit):
         if deposit > 0:
             self.balance += deposit
         else:
-            print(f"You may deposit positive amount only. ${deposit} is not" +
+            raise ValueError(f"You may deposit positive amount only. ${deposit} is not" +
                   " valid.")
 
-# if __name__ == "__main__":
-#     """Run tests if module is executed by name."""
-#     account = Account()
+if __name__ == "__main__":
+    """Run tests if module is executed by name."""
+    account = Account()
+    print(account.number, account.balance, account.limit)
