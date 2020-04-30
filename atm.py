a97699bbaa284e011bca6be5f62af5fece0947a3
Dave Braunschweig <dave@chicagocoding.com>
2020-04-30 14:27:47 -0500

    Customer integrated

--- atm.py
+++ atm.py
@@ -14,6 +14,7 @@ import customer
 class Atm(object):
     _id = "12345"
     _account = None
+    _customer = None
 
     @property
     def id(self):
@@ -26,7 +27,6 @@ class Atm(object):
     def __init__(self):
         print("Atm initialized")
         self.display_welcome()
-        self._account = account.Account()
 
     def display_welcome(self):
         # This should be a console.welcome rather than a direct print.
@@ -35,6 +35,12 @@ class Atm(object):
     def deposit(self, deposit):
         self._account.deposit(deposit)
     
+    def log_in(self, card_id, pin):
+        self._customer = customer.Customer()
+        self._customer.validate(card_id, pin)
+        self._account = account.Account()
+
+
     def withdrawal(self, withdrawal):
         self._account.withdrawal(withdrawal)
     
@@ -42,6 +48,8 @@ class Atm(object):
 if __name__ == "__main__":
     """ testing """
     atm = Atm()
+    atm.log_in("1111111111", "1111")
+
     atm.deposit(100)
     print(atm.balance)
     atm.withdrawal(50)
