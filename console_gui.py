1a20865bfe55c756cf9ddaa54f9ab98bcdcde81d
w_workman1 <w_workman1@mail.harpercollege.edu>
2020-04-30 20:40:51 -0500

    Added entry fields and a submit button for deposit, withdraw, transfer and account

--- console_gui.py
+++ console_gui.py
@@ -1,6 +1,40 @@
 import tkinter as tk
 from tkinter import *
 
+keys = [
+    ['7', '8', '9'],
+    ['4', '5', '6'],
+    ['1', '2', '3'],
+    ['←', '0', '.']
+]
+numbers = ''
+
+root = tk.Tk()
+root.title("Console")
+
+deposit = Frame(root)
+withdraw = Frame(root)
+transfer = Frame(root)
+account = Frame(root)
+
+entry = tk.Entry(root)
+entry.grid(row=0, column=0, columnspan=3, ipady=5)
+for y, row in enumerate(keys, 1):
+    for x, key in enumerate(row):
+        buttons = tk.Button(root, text=key, command=lambda val=key: numpad(val))
+        buttons.grid(row=y, column=x, ipadx=10, ipady=10)
+
+    for frame in (deposit, withdraw, transfer, account):
+        frame.grid(row=0, column=0)
+    button1 = tk.Button(root, text="Deposit", command=lambda: deposit())
+    button2 = tk.Button(root, text="Withdraw", command=lambda: withdraw())
+    button3 = tk.Button(root, text="Transfer", command=lambda: transfer())
+    button4 = tk.Button(root, text="Account", command=lambda: account())
+    button1.grid(row=1, column=4)
+    button2.grid(row=2, column=4)
+    button3.grid(row=3, column=4)
+    button4.grid(row=4, column=4)
+
 
 def numpad(value):
     global numbers
@@ -14,70 +48,61 @@ def numpad(value):
         print(numbers)
 
 
-keys = [
-    ['7', '8', '9'],
-    ['4', '5', '6'],
-    ['1', '2', '3'],
-    ['←', '0', '.']
-]
-numbers = ''
+def enter_id():
+    window = Tk()
+    Label(window, text="Enter card ID: ").grid(row=0, column=1)
+    e1 = Entry(window)
+    e1.grid(row=1, column=0, columnspan=3, ipady=5)
+    window.mainloop()
 
-root = tk.Tk()
-deposit = Frame(root)
-withdraw = Frame(root)
-transfer = Frame(root)
-account = Frame(root)
 
-entry = tk.Entry(root)
-entry.grid(row=0, column=0, columnspan=3, ipady=5)
-for y, row in enumerate(keys, 1):
-    for x, key in enumerate(row):
-        buttons = tk.Button(root, text=key, command=lambda val=key: numpad(val))
-        buttons.grid(row=y, column=x, ipadx=10, ipady=10)
-    for frame in (deposit, withdraw, transfer, account):
-        frame.grid(row=0, column=0)
-    button1 = tk.Button(root, text="Deposit", command=lambda: deposit())
-    button2 = tk.Button(root, text="Withdraw", command=lambda: withdraw())
-    button3 = tk.Button(root, text="Transfer", command=lambda: transfer())
-    button4 = tk.Button(root, text="Account", command=lambda: account())
-    button1.grid(row=1, column=4)
-    button2.grid(row=2, column=4)
-    button3.grid(row=3, column=4)
-    button4.grid(row=4, column=4)
+def enter_pin():
+    window = Tk()
+    Label(window, text="Enter PIN: ").grid(row=0, column=1)
+    e1 = Entry(window)
+    e1.grid(row=1, column=0, columnspan=3, ipady=5)
+    window.mainloop()
 
 
 def deposit():
-    window = Tk()
-    label = tk.Label(window, text="Deposit", font=('Helvetica', 18), pady=10)
-    label.grid(pady=10)
-    # Put in prompt and entry
+    window = tk.Tk()
+    window.title("Deposit")
+    Label(window, text="Enter amount to deposit: ").grid(row=0, column=1)
+    e1 = Entry(window)
+    e1.grid(row=1, column=0, columnspan=3, ipady=5)
+    button_deposit = Button(window, text="Submit")
+    button_deposit.grid(row=2, column=1)
     window.mainloop()
 
 
 def withdraw():
-    window = Tk()
-    label = tk.Label(window, text="Withdraw", font=('Helvetica', 18), pady=10)
-    label.grid(pady=10)
-    # Put in prompt and entry
+    window = tk.Tk()
+    window.title("Withdraw")
+    Label(window, text="Enter amount to withdraw: ").grid(row=0, column=1)
+    e1 = Entry(window)
+    e1.grid(row=1, column=0, columnspan=3, ipady=5)
+    button_withdraw = Button(window, text="Submit")
+    button_withdraw.grid(row=2, column=1)
     window.mainloop()
 
 
 def transfer():
-    window = Tk()
-    label = tk.Label(window, text="Transfer", font=('Helvetica', 18), pady=10)
-    label.grid(pady=10)
-    # Put in prompt and entry
+    window = tk.Tk()
+    window.title("Transfer")
+    Label(window, text="Enter amount to transfer: ").grid(row=0, column=1)
+    e1 = Entry(window)
+    e1.grid(row=1, column=0, columnspan=3, ipady=5)
+    button_transfer = Button(window, text="Submit")
+    button_transfer.grid(row=2, column=1)
     window.mainloop()
 
 
 def account():
-    window = Tk()
+    window = tk.Tk()
+    window.title("Account")
     label = tk.Label(window, text="Account", font=('Helvetica', 18), pady=10)
     label.grid(pady=10)
-    # Put in prompt and entry
     window.mainloop()
 
 
 root.mainloop()
-
-
